5e49a84ee9abb7a75dd198099dea3c756f307466
Phan Anh Tuấn <57442280+PATCoder97@users.noreply.github.com>
2019-11-07 13:14:56 +0700

    Test Edit code

--- Tool_Watch_And_Move/Form1.cs
+++ Tool_Watch_And_Move/Form1.cs
@@ -23,6 +23,7 @@ namespace Tool_Watch_And_Move
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        //Test ha
             string filePath = Application.StartupPath + "\\Temp.txt";
 
             if (File.Exists(filePath))
